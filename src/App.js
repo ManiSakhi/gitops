776c5ee0317be9600e5f42516f4299ddefc22c6d
ManiSakhi <96877629+ManiSakhi@users.noreply.github.com>
2023-09-27 12:53:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>Hello all!!!</span>
+          <span style={{ color: 'red' }}>Hello, Testing gitops using Argocd!!!</span>
         </div>
       </header>
     </div>
